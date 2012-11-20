6a0006685f9a704371a03d47b59e26d6b78a3e78
Derek Keats <derek@dkeats.com>
2012-11-20 19:31:37 +0000

    Spelling correction

--- tu/templates/page/page_template.php
+++ tu/templates/page/page_template.php
@@ -269,7 +269,7 @@ $this->putMessages();
 function getIcons($skinName, $canvas)
 {
     $available=array(
-        'tu' => 'http://thumbzip.com',
+        'tu' => 'http://thumbzup.com',
         'facebook' => '#',
         'flickr' => '#',
         'twitter' => '#',
